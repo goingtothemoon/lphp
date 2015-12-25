73f3b41da8c7b89a8058083bfa31fcab5e6d27c6
Richard Fernandez <kcomico@gmail.com>
2015-12-25 15:20:07 -0500

    Update

---
+++ helloworld.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Hello World</title>
+</head>
+
+<body>
+	<?php echo "Hello World!";
+	/* The echo() function outputs one or more strings */ ?>
+
+	<?php echo "Hello"."World!"; 
+	/* The . concatenates	*/?>
+
+	<?php echo 2 + 3; 
+	/* You must echo for it to show the result.
+	    The ; is a separator */?>
+
+	<?php 
+	// Single line comments can be done using //
+
+	# Can also be used
+	
+	/* Double line comments so you can continue
+	   for a longer comment. */
+	?>
+
+</body>
+
+
+</html>
