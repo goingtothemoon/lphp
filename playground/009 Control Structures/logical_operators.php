b2b35c3ae055a24ec564056cd8cfa7a393935488
Richard Fernandez <kcomico@gmail.com>
2015-12-26 11:19:04 -0500

    update

--- playground/009 Control Structures/logical_operators.php
+++ playground/009 Control Structures/logical_operators.php
@@ -5,7 +5,48 @@
 </head>
 <body>
 
-	<?php  ?>
+	<b>Comparison and Logical Operators</b> <br />
+	Equal: == <br />
+	Identical: === <br />
+	Compare: > < >= <= <> <br />
+	Not equal: != <br />
+	Not identical: !== <br /><br />
+
+	<b>Logical Operators</b> <br />
+	and: && <br />
+	or: || <br />
+	not: ! <br />
+----------------------------------------------- <br />
+
+	<?php 
+
+	$a = 4;
+	$b = 3;
+	$c = 10;
+	$d = 1;
+
+	if (($a > $b) && ($c > $d)) {
+		# code...
+		echo "a is larger than b AND ";
+		echo "c is larger than d";
+	}
+
+	?><br /><br />
+
+	<?php 
+
+	$a = 4;
+	$b = 3;
+	$c = 1;
+	$d = 10;
+
+	if (($a > $b) || ($c > $d)) {
+		# code...
+		echo "a is larger than b or ";
+		echo "c is larger than d";
+	}
+
+	?>
 	
 </body>
 </html>
