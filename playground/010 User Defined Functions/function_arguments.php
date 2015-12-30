def6e8c32d1513937f817385442fe4739d9a77ea
Richard Fernandez <kcomico@gmail.com>
2015-12-29 23:25:39 -0500

    update

--- playground/010 User Defined Functions/function_arguments.php
+++ playground/010 User Defined Functions/function_arguments.php
@@ -11,10 +11,36 @@
 		function say_hello_to($word) {
 			echo "Hello {$word}!<br /> ";
 		}
+			// function has its own world
 			$name = "Jogn Doe"
 		say_hello_to("$name");
 
 	 ?>
+
+	 <?php 
+	 	// function with more than one argument
+	 	// str_replace("quick", "super-fast", $third);
+
+	 	// // relation
+	 	// function str_replace($find, $replace, $target){
+
+	 	// }
+
+
+	  ?>
+
+	  <?php 
+
+	  	function better_hello($greeting, $target, $punct) {
+	  		echo $greeting . " " . $target . $punct . "<br />";
+	  	}
+	  		better_hello("Hello", $name, "!");
+	  		better_hello("Alo", $name, "!!!");
+	  		
+	  		better_hello("Hello", $name, null);
+
+	   ?>
+
 	
 </body>
 </html>
---
+++ playground/010 User Defined Functions/returningValue.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Returning Value</title>
+</head>
+<body>
+
+	<?php 
+
+		function add($var1 , $var2) {
+			$sum = $var2 + $var2;
+		}
+
+	 ?>
+
+	
+</body>
+</html>
