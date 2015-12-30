a286ec3c7a01ad9c504ca4e37af906d22111f3be
Richard Fernandez <kcomico@gmail.com>
2015-12-29 19:42:11 -0500

    update

---
+++ playground/010 User Defined Functions/defFunction.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Defining Functions</title>
+</head>
+<body>
+
+	<?php 
+
+		// Function: A code that preforms a specific task ex: inarry
+
+		function say_hello() {
+			echo "Hello World!<br />";
+		}
+
+		// Call function
+		// Must use ()
+		say_hello();
+
+
+		function say_hello_to($word) {
+			echo "Hello {$word}!<br />";
+		}
+
+		say_hello_to("World");
+		say_hello_to("Friend");
+
+		function say_hello_caps() {
+			echo "HELLO";
+		}
+
+		say_hello_caps();
+
+	 ?>
+	
+</body>
+</html>
