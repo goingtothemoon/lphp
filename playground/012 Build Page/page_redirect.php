fb5e9c2e53b8a57c2285bde958b6c679ff9e212d
Richard Fernandez <kcomico@gmail.com>
2016-01-06 23:14:19 -0500

    update

--- playground/012 Build Page/page_redirect.php
+++ playground/012 Build Page/page_redirect.php
@@ -1,7 +1,24 @@
 <?php 
-// This is how you redirect to a new page
-header("Location: first_page.php");
-exit; // PHP is complete
+
+// // This is how you redirect to a new page
+// header("Location: first_page.php");
+// exit; // PHP is complete
+
+# Using function for same use
+
+function redirect_to($new_location) {
+	header("Location: " . $new_location);
+	exit;
+}
+
+$logged_in = $_GET['logged_in']; // $_GET : An associative array of variables passed to the current script via the URL parameters.
+
+if ($logged_in == "1") {
+	# code...
+	redirect_to("http://www.yahoo.com");
+} else {
+	redirect_to("http://www.google.com");
+}
 
  ?>
 
