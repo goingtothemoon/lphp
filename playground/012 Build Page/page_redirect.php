67a614304f683b428f2bd470d5a6c6b9c4c13357
Richard Fernandez <kcomico@gmail.com>
2016-01-06 23:02:09 -0500

    update

---
+++ playground/012 Build Page/page_redirect.php
@@ -0,0 +1,35 @@
+<?php 
+// This is how you redirect to a new page
+header("Location: first_page.php");
+exit; // PHP is complete
+
+ ?>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Page redirection</title>
+</head>
+<body>
+
+	<?php 
+
+		// Page redirection
+
+		/*
+			# Sending person to different page.
+			
+			#302 Redirect
+			-HTTP 1.1/302 Found
+			-Location: path
+
+			# header("Location: login.php");
+
+
+		*/
+
+
+	 ?>
+	
+</body>
+</html>
