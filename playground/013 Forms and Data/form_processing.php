bd7ef88035147ee0c96000a50a88f09cedf7ecbd
Richard Fernandez <kcomico@gmail.com>
2016-01-08 23:29:51 -0500

    update

--- playground/013 Forms and Data/form_processing.php
+++ playground/013 Forms and Data/form_processing.php
@@ -11,21 +11,51 @@
 		 ?>
 	</pre>
 	<br />
+
 	<?php 
-		if(isset($_POST]"username"])){  // isset — Determine if a variable is set and is not NULL
+		// set default values
+
+		if (isset($_POST["username"])) {  // isset — Determine if a variable is set and is not NULL
 			# code...
 		$username = $_POST["username"]; // Easier to work with instead of always using superglobal
+		} else {
+			$username = ""; // Default
 		}
 		if (isset($_POST["password"])) {
 			# code...
 			$password = $_POST["password"];
+		} else {
+			$password = ""; // Default
 		}
+	?>
+
+	<?php 
+		// set default values using ternary operator (faster way)
+		// # Another conditional operator is the "?:" (or ternary) operator.
+
+		$username = isset($_POST['username']) ? $_POST['username'] : "";
+		$password = isset($_POST['password']) ? $_POST['password'] : "";
+	 ?>
+	
+	<?php 
+		// detect form submission
+		// form.php line 18; must have name submit for this to work
+
+		if (isset($_POST['submit'])) {
+			echo "form was submitted";
+		}
+
 		
 
+	 ?>
 
+
+
+	<?php
 		echo "{$username}: {$password}";
 
 		// Post data does not need to be incoded or decoded
+
 	 ?>
 	
 </body>
