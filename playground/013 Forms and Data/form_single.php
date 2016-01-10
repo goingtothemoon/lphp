fbc78e5832265df305cb3ce4c6a2d8d0d9474b81
Richard Fernandez <kcomico@gmail.com>
2016-01-09 21:09:15 -0500

    update

--- playground/013 Forms and Data/form_single.php
+++ playground/013 Forms and Data/form_single.php
@@ -1,10 +1,22 @@
 <?php 
+// redirect_to("www.google.com");
+
+
 if (isset($_POST['submit'])) {
 	// form was submitted
 	$username = $_POST["username"];
 	$password = $_POST["password"];
-	$message = "Logging in: {$username}";
-} else {
+
+	if ($username == "person" && $password == "person") {
+		# Login good
+		redirect_to("www.google.com");
+		
+	} else {
+
+		$message = "There were some errors.";
+		}
+	} else {
+	$username = "";
 	$message = "Please log in.";
 }
 
@@ -21,7 +33,8 @@ if (isset($_POST['submit'])) {
 <!-- This is going to post the data to form_processing.php // Method we are using is post -->
 	<form action="form_single.php" method="post">
 		<!-- Inside post there will be a value for username -->
-		Username: <input type="text" name="username" value="" /><br />
+		Username: <input type="text" name="username" value="<?php echo 
+		htmlspecialchars($username); ?>" /><br />
 		<!-- Type: password which shows bullets instead of text -->
 		Password: <input type="password" name="password" value="" /><br />
 		<br />
